217cddafa7adde3a4b010d37a8a1f6871711cbf2
Malik Khan <malik.khan@kcl.ac.uk>
2023-10-24 19:09:41 +0100

    Done

--- employee.py
+++ employee.py
@@ -2,30 +2,73 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 class Employee:
-    def __init__(self, name):
+    def __init__(self, name, salary_type, **kwargs):
         self.name = name
+        self.salary_type = salary_type
+        self.additional_info = kwargs
 
     def get_pay(self):
-        pass
+        if self.salary_type == 'monthly':
+            return 4000
+        elif self.salary_type == 'contract':
+            hours_worked = self.additional_info.get('hours_worked', 0)
+            hourly_rate = self.additional_info.get('hourly_rate', 0)
+            return hours_worked * hourly_rate
+        elif self.salary_type == 'monthly_with_commission':
+            base_salary = 3000
+            commission_rate = self.additional_info.get('commission_rate', 0)
+            num_contracts = self.additional_info.get('num_contracts', 0)
+            return base_salary + (commission_rate * num_contracts)
+        elif self.salary_type == 'contract_with_commission':
+            hours_worked = self.additional_info.get('hours_worked', 0)
+            hourly_rate = self.additional_info.get('hourly_rate', 0)
+            commission_rate = self.additional_info.get('commission_rate', 0)
+            num_contracts = self.additional_info.get('num_contracts', 0)
+            return (hours_worked * hourly_rate) + (commission_rate * num_contracts)
+        elif self.salary_type == 'monthly_with_bonus':
+            base_salary = 2000
+            bonus = self.additional_info.get('bonus', 0)
+            return base_salary + bonus
+        elif self.salary_type == 'contract_with_bonus':
+            hours_worked = self.additional_info.get('hours_worked', 0)
+            hourly_rate = self.additional_info.get('hourly_rate', 0)
+            bonus = self.additional_info.get('bonus', 0)
+            return (hours_worked * hourly_rate) + bonus
+        else:
+            return 0  # Unknown salary type
 
     def __str__(self):
-        return self.name
+        if self.salary_type == 'monthly':
+            return f"{self.name} works on a monthly salary of {self.get_pay()}. Their total pay is {self.get_pay()}."
+        elif self.salary_type == 'contract':
+            hours_worked = self.additional_info.get('hours_worked', 0)
+            hourly_rate = self.additional_info.get('hourly_rate', 0)
+            return f"{self.name} works on a contract of {hours_worked} hours at {hourly_rate}/hour. Their total pay is {self.get_pay()}."
+        elif self.salary_type == 'monthly_with_commission':
+            base_salary = 3000
+            commission_rate = self.additional_info.get('commission_rate', 0)
+            num_contracts = self.additional_info.get('num_contracts', 0)
+            return f"{self.name} works on a monthly salary of {base_salary} and receives a commission for {num_contracts} contract(s) at {commission_rate}/contract. Their total pay is {self.get_pay()}."
+        elif self.salary_type == 'contract_with_commission':
+            hours_worked = self.additional_info.get('hours_worked', 0)
+            hourly_rate = self.additional_info.get('hourly_rate', 0)
+            commission_rate = self.additional_info.get('commission_rate', 0)
+            num_contracts = self.additional_info.get('num_contracts', 0)
+            return f"{self.name} works on a contract of {hours_worked} hours at {hourly_rate}/hour and receives a commission for {num_contracts} contract(s) at {commission_rate}/contract. Their total pay is {self.get_pay()}."
+        elif self.salary_type == 'monthly_with_bonus':
+            base_salary = 2000
+            bonus = self.additional_info.get('bonus', 0)
+            return f"{self.name} works on a monthly salary of {base_salary} and receives a bonus commission of {bonus}. Their total pay is {self.get_pay()}."
+        elif self.salary_type == 'contract_with_bonus':
+            hours_worked = self.additional_info.get('hours_worked', 0)
+            hourly_rate = self.additional_info.get('hourly_rate', 0)
+            bonus = self.additional_info.get('bonus', 0)
+            return f"{self.name} works on a contract of {hours_worked} hours at {hourly_rate}/hour and receives a bonus commission of {bonus}. Their total pay is {self.get_pay()}."
 
-
-# Billie works on a monthly salary of 4000.  Their total pay is 4000.
-billie = Employee('Billie')
-
-# Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
-charlie = Employee('Charlie')
-
-# Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
-renee = Employee('Renee')
-
-# Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
-jan = Employee('Jan')
-
-# Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
-robbie = Employee('Robbie')
-
-# Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
-ariel = Employee('Ariel')
+# Create instances of employees with additional information
+billie = Employee('Billie', 'monthly')
+charlie = Employee('Charlie', 'contract', hours_worked=100, hourly_rate=25)
+renee = Employee('Renee', 'monthly_with_commission', num_contracts=4, commission_rate=200)
+jan = Employee('Jan', 'contract_with_commission', hours_worked=150, hourly_rate=25, num_contracts=3, commission_rate=220)
+robbie = Employee('Robbie', 'monthly_with_bonus', bonus=1500)
+ariel = Employee('Ariel', 'contract_with_bonus', hours_worked=120, hourly_rate=30, bonus=600)
